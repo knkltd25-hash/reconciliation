54edd43050b39cd4dcdf44d003b1dc888e900406
knkltd25-hash <sckousik@gmail.com>
2026-02-24 14:46:38 +0530

    enhancements

--- frontend/src/utils/backend.js
+++ frontend/src/utils/backend.js
@@ -1,3 +1,3 @@
 // Utility to get backend URL and port from environment variables
 export const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT || '8000';
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || `http://localhost:${BACKEND_PORT}`;
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || `http://54.145.92.198:${BACKEND_PORT}`;
